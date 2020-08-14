640f21671d81da58db5ebed0dde002f0eaeb950c
石浩博 <42900775+626sy@users.noreply.github.com>
2020-08-14 09:51:38 +0800

    LeetCode

---
+++ src/com/neusoft/LeetCode/Solution.java
@@ -0,0 +1,28 @@
+package com.neusoft.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @author shihaobo
+ * @date 2020/8/14 8:50
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(new Solution().twoSum(nums, target)));
+
+
+    }
+    public int[] twoSum(int[] nums,int target){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+
+}
