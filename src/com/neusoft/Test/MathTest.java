b4269681dae9f097382c017a426a2335b71c24cf
石浩博 <42900775+626sy@users.noreply.github.com>
2020-09-21 08:52:43 +0800

    测试

---
+++ src/com/neusoft/Test/MathTest.java
@@ -0,0 +1,19 @@
+package com.neusoft.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author shihaobo
+ * @date 2020/9/19 14:47
+ */
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println(Math.round(11.5));
+        System.out.println(Math.floor(-11.5));
+        ArrayList list = new ArrayList(20);
+        char c = 'a';
+    }
+
+}
