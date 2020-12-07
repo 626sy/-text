3ca214a36d18ecf3ac97fbce6c61911f521c7c47
石浩博 <42900775+626sy@users.noreply.github.com>
2020-12-07 09:54:47 +0800

    Create anshuo.java

---
+++ src/com/neusoft/anshuo.java
@@ -0,0 +1,39 @@
+package com.neusoft;
+
+/**
+ * @author shihaobo
+ * @date 2020/10/10 19:55
+ */
+public class anshuo {
+//    int x,y;
+//    void fly(int x,int y){
+//        this.x=x;
+//        this.y=y;
+//    }
+    static int arr[] = new int[10];
+
+    public static void main(String[] args) {
+//        System.out.println(arr[1]);
+//        int i = 0;
+//        switch (i){
+//            case 0:
+//                System.out.println("ling");
+//            case 1:
+//                System.out.println("yi");
+//            case 2:
+//                System.out.println("er");
+//                break;
+//            case 3:
+//                System.out.println("san");
+//
+//        }
+//        for (int j = 0; j < arr.length; j++) {
+//
+//        }
+        int x=20,y=30;
+        int k=(x>y) ? y: x;
+        System.out.println(k);
+    }
+//    StringBuffer sb = "这里是";
+
+}
